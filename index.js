8f39c01055688b5741ec86d2d65cf54f8057374c
David J Towster <djt4xe@virginia.edu>
2017-11-28 20:38:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicians.length; i++) {
-    array.push(`John Lennon plays guitar`);
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
   }
 }
