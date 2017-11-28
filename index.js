fc136a5de9e98872381609acf06ae777b64341f9
David J Towster <djt4xe@virginia.edu>
2017-11-28 20:46:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number) {
   var array = [];
   do {
-    array.push('')
-  } while ();
+    array.push('I love the Beatles!')
+    number++;
+  } while (number < 15);
+  return array;
 }
